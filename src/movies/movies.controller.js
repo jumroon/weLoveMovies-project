4c0890352c896cc5a2149aaf3d0ed3b7c58f7361
jumroon <jumroon.rada@gmail.com>
2022-12-02 00:44:43 +0700

    fix: movies controller and service

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,8 +2,8 @@ const service = require("./movies.service");
 const asyncErrorBoundary = require("../utils/errors/asyncErrorBoundary");
 
 async function list(request, response, next) {
-  // const result = await service.list;
-  response.status(200).json({ data: { result1: "result" } });
+  const result = await service.list;
+  response.status(200).json({ data: { result: result } });
 }
 
 module.exports = {
