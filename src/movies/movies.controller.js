ffcd6a9c31a9c33cc2ff8d94d1d549450b1187ec
jumroon <jumroon.rada@gmail.com>
2022-12-02 22:41:16 +0700

    shows movies that are currently showing in a theater

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,7 +2,14 @@ const service = require("./movies.service");
 const asyncErrorBoundary = require("../utils/errors/asyncErrorBoundary");
 
 async function list(request, response, next) {
-  const result = await service.list();
+  const { is_showing } = request.query; //ask to explain/work on destructuring a bit more
+  let result = null;
+  if (is_showing) {
+    result = await service.listShowings();
+  } else {
+    result = await service.list();
+  }
+
   response.status(200).json({ data: result });
 }
 
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -4,4 +4,15 @@ function list() {
   return knex("movies").select("*");
 }
 
-module.exports = { list };
+function listShowings() {
+  const subquery = knex("movies_theaters")
+    .distinct("movie_id")
+    .where({ is_showing: true })
+    .as("movies_shown");
+
+  return knex("movies").join(subquery, {
+    "movies_shown.movie_id": "movies.movie_id",
+  });
+}
+
+module.exports = { list, listShowings };
