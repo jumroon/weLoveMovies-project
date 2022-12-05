2c135c647d5946726d47a9867dd3fdf7864a68a8
jumroon <jumroon.rada@gmail.com>
2022-12-05 15:47:07 +0700

    cleaned up code

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,29 +1,5 @@
 const knex = require("../db/connection");
 
-// function list() {
-//   const subquery = knex("movies_theaters")
-//     .select("theater_id", "movie_id")
-//     .groupBy("theater_id")
-//     .as("movies_shown");
-
-//   knex("theaters").join(subquery, {
-//     "movies_shown.theater_id": "theaters.theater_id",
-//   });
-
-//   return knex("theaters").select("*");
-// }
-
-// function listShowings() {
-//   const subquery = knex("movies_theaters")
-//     .distinct("movie_id")
-//     .where({ is_showing: true })
-//     .as("movies_shown");
-
-//   return knex("movies").join(subquery, {
-//     "movies_shown.movie_id": "movies.movie_id",
-//   });
-// }
-
 function list() {
   return knex("movies_theaters as mt")
     .join("movies as m", "m.movie_id", "mt.movie_id")
