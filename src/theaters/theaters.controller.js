3a4e9a8885721965ef6080319fd4b1af1561018c
jumroon <jumroon.rada@gmail.com>
2022-12-05 19:21:09 +0700

    theater test passing

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -3,9 +3,59 @@ const asyncErrorBoundary = require("../utils/errors/asyncErrorBoundary");
 
 async function list(request, response, next) {
   const result = await service.list();
-  console.log("result", result);
-  response.status(200).json({ data: result });
+  // console.log("result", result);
+
+  const testResult = result.map((movie) => {
+    return {
+      name: movie.name,
+      address_line_1: movie.address_line_1,
+      address_line_2: movie.address_line_2,
+      city: movie.city,
+      state: movie.state,
+      zip: movie.zip,
+      movies: [
+        {
+          title: movie.title,
+          runtime_in_minutes: movie.runtime_in_minutes,
+          rating: movie.rating,
+        },
+      ],
+    };
+  }, {});
+
+  const newArray = [];
+
+  for (let i = 0; i < testResult.length; i++) {
+    if (newArray.find((theater) => theater.name === testResult[i].name)) {
+      console.log("here");
+      //find in the new array the object with the same name, and add the movie of testResult[i] to the movies array of this object
+      newArray
+        .find((obj) => obj.name === testResult[i].name)
+        .movies.push(testResult[i].movies[0]);
+    } else {
+      newArray.push(testResult[i]);
+    }
+  }
+
+  console.log(newArray);
+  response.status(200).json({ data: newArray });
 }
+
+// const expectedHollywoodTheatre = {
+//   name: "Hollywood Theatre",
+//   address_line_1: "4122 NE Sandy Blvd.",
+//   address_line_2: "",
+//   city: "Portland",
+//   state: "OR",
+//   zip: "97212",
+//   movies: expect.arrayContaining([
+//     expect.objectContaining({
+//       title: "Spirited Away",
+//       runtime_in_minutes: 125,
+//       rating: "PG",
+//     }),
+//   ]),
+// };
 
 module.exports = {
   list: asyncErrorBoundary(list),
